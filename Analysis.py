4275443deb635cdc971cbe3471f9bde928932e96
Shen Zhang <szhang5@clemson.edu>
2019-04-23 19:14:12 -0400

    Analysis Report

--- Analysis.py
+++ Analysis.py
@@ -11,9 +11,9 @@ from sklearn.ensemble import BaggingClassifier
 ###### Change directory
 os.chdir("/Users/shen/Desktop/Homework/parsed_results/")
 
-dataset = pd.read_csv("BoardGameGeekClean.csv")
+dataset = pd.read_csv("BoardGameGeekClean_Miss.csv")
 
-np.any(np.isnan(dataset))
+#np.any(np.isnan(dataset))
 #print(dataset.shape)
 ######################### RandomForest Analysis ####################
 target = dataset.iloc[:,7].values
@@ -21,21 +21,36 @@ target = dataset.iloc[:,7].values
 data = dataset[['AvgRating','GeekRating', 'ListPrice', 'NumVoters']]
 
 data_training, data_test, target_training, target_test = train_test_split(data,target, test_size = 0.2, random_state = 1)
-
+	
 
 
 random_forest_machine = RandomForestClassifier(n_estimators=11)
 
 random_forest_machine.fit(data_training,target_training)
 
+predictions = random_forest_machine.predict(data_test)
 
 print(accuracy_score(target_test,predictions))
 
-#print(data.head())
-	
+
+confusion_matrix = pd.DataFrame(
+	confusion_matrix(target_test,predictions),
+	columns = ['predict 1', 'Predict 2', 'Predict 3','Predict 4'],
+	index = ['True 1','True 2','True 3','True 4' ]
+	)
+		
+
+print(confusion_matrix)
+
+print(dict(zip(data.columns, 
+	random_forest_machine.feature_importances_)))
 
 
-
+######## Data set without the Missing Values #######
+#### We do the exactly same thing but without the information of Missing value
+### We use the data set 'BoardGameGeekClean_Miss.csv' which do not contain missing value.
+## We also get the accuracy_score and confusion matrix
+## Code is same but switch the dataset 
 
 
 
--- test.py
+++ test.py
@@ -1,29 +1,58 @@
-import urllib.request
+import numpy as np
+import pandas as pd 
 import os
-import ssl
-import re
-from bs4 import BeautifulSoup
-import time
-import datetime
-from urllib.request import urlopen as uReq
-import requests
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from sklearn.ensemble import BaggingClassifier
+from sklearn.neighbors import KNeighborsClassifier
 
-unverified_context = ssl._create_unverified_context()
-
-web_url = 'https://boardgamegeek.com/browse/boardgame/page/2'
-response = requests.get(web_url)
-#page_html = uClient.read()
-#uClient.close()
-
-
-#response = urllib.request.urlopen(web_url, context = unverified_context)
+###### Change directory
+os.chdir("/Users/shen/Desktop/Homework/parsed_results/")
+	
+######## Data set without the Missing Values #######
+#### We do the exactly same thing but without the information of Missing value
 
 
-page_soup = BeautifulSoup(response.text, 'xml')
+dataset_Miss = pd.read_csv("BoardGameGeekClean.csv")
+
+#np.any(np.isnan(dataset))
+#print(dataset.shape)
+######################### RandomForest Analysis ####################
+target = dataset_Miss.iloc[:,7].values
+
+data = dataset_Miss[['AvgRating','GeekRating', 'ListPrice', 'NumVoters']]
+
+data_training, data_test, target_training, target_test = train_test_split(data,target, test_size = 0.2, random_state = 1)
+	
+knn = KNeighborsClassifier(n_neighbors=6)
+
+knn.fit(data_training, target_training)
+
+#random_forest_machine.fit(data_training,target_training)
+
+predictions = knn.predict(data_test)
+
+print(accuracy_score(target_test,predictions))
 
 
-#response = urllib.request.urlopen(web_url, context = unverified_context)
+confusion_matrix2 = pd.DataFrame(
+	confusion_matrix(target_test,predictions),
+	columns = ['predict 1', 'Predict 2', 'Predict 3','Predict 4'],
+	index = ['True 1','True 2','True 3','True 4' ]
+	)
+		
 
-#soup = BeautifulSoup(response, 'html.parser')
+print(confusion_matrix2)
 
-print(page_soup)
+#print(dict(zip(data.columns, 
+#	knn.feature_importances_)))
+
+
+
+
+
+
+
