9b800c005e925bf41a8b76bf497e0429e72b8fe6
Shen Zhang <szhang5@clemson.edu>
2019-04-23 04:24:59 -0400

    Get Parsed Results

--- Homework_parse.py
+++ Homework_parse.py
@@ -2,38 +2,76 @@ import os
 from bs4 import BeautifulSoup
 import glob
 import pandas as pd
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+
+if not os.path.exists("parsed_results"):
+	os.mkdir("parsed_results")
+
+os.chdir("/Users/shen/Desktop/Homework/")
+
+#os.chdir("/Users/shen/Desktop/Homework/html_files")
 
 
-cwd = os.getcwd()
-
-print(cwd)
-
-driver = webdriver.Safari()
+df = pd.DataFrame()
 
 
-driver.get("https://boardgamegeek.com/browse/boardgame/page/1")
+for one_file_name in glob.glob("html_files/*.html"):
 
-html = driver.page_source
+	print("parsing"+ one_file_name)
+	f = open(one_file_name, "r")
+	soup = BeautifulSoup(f.read(), 'html.parser')
+	f.close()
 
-print(html)
+	
+	table_All = soup.find_all("tr", {"id":"row_"})
 
-#cwd = os.getcwd()
-#print(cwd)
-## command + d 
-#if not os.path.exists("parsed_results"):
-#	os.mkdir("parsed_results")
+	#print(table_All)
 
-#one_file_name = "Boardgamegeek1.html"
+	for r in table_All:
+		#print(r.find('div',{"id":"results_objectname1"}).find('a').text)
 
-#os. chdir("/Users/shen/Desktop/Homework/html_files")
+		collection_name = r.find("td", {"class":"collection_objectname"}).find('a').text
+		
+		collection_rank = r.find('td',{"class":"collection_rank"}).text
 
-#f = open(one_file_name, "r")
-#soup = BeautifulSoup(f.read(), 'html.parser')
-#f.close()
+		collection_rating = r.find("td",{"class":"collection_bggrating"}).text
 
-#table_All = soup.find_all("tr", {"id":"row_"})
+		collection_avg_rating = r.find("td",{"class":"collection_bggrating"}).findNext("td",{"class":"collection_bggrating"}).text
+
+
+		collection_voters = r.find("td",{"class":"collection_bggrating"}).findNext("td",{"class":"collection_bggrating"}).findNext("td",{"class":"collection_bggrating"}).text
+
+		#print(r.find("td",{"class":"collection_shop"}).find("div",{"class":"aad"}).find('div').find('div').text)
+
+		collection_price=r.find("td",{"class": "collection_shop"}).find("div", {"class": "aad"})
+
+		# Simply get next tag Or we can search key word List price to collect data
+		if collection_price is None:
+			continue
+		else:
+			collection_Listprice = collection_price.next_element.next_element.next_element.next_element
+			
+			#print(collection_Listprice)
+
+		#collection_Listprice = r.find("td",{"class":"collection_shop"}).find("div",{"class":"aad"}).find('div').find('div').text
+
+
+		df = df.append({
+				'GameName':collection_name,
+				'GameRank':collection_rank,
+				'GeekRating':collection_rating,
+				'AvgRating':collection_avg_rating,
+				'NumVoters': collection_voters,
+				'ListPrice': collection_Listprice,
+			
+				} ,ignore_index=True)
+
+
+
+#os.chdir("/Users/shen/Desktop/Homework/parsed_results/")
+
+df.to_csv("parsed_results/BoardGameGeek.csv")
+
+#print(type(collection_Listprice))
 
 #table_shop = soup.find_all("td", {"class":"collection_shop"})
 
--- Homework_scraping.py
+++ Homework_scraping.py
@@ -11,17 +11,17 @@ os.chdir("/Users/shen/Desktop/Homework/html_files/")
 #### In mac system, safari has its own webdriver
 driver = webdriver.Safari()
 
-for x in range(1062):
-	#### Get index from 1 to 1062
+for x in range(1066):
+	#### Get index from 1 to 1066
 	x = x+1
 	#### Use the dynamic method to grab the data from website
 	driver.get("https://boardgamegeek.com/browse/boardgame/page/" +str(x)) 
 	page_html = driver.page_source 
 
-	#### Write the web into files and a 20 second stop between each collection
+	#### Write the web into files and a 10 second stop between each collection
 	f = open("Boardgamegeek" + str(x)+".html", "w") 
 	f.write(page_html)
 	f.close()
-	time.sleep(20)
+	time.sleep(10)
 
 driver.close()
